243e2c685aaf23b58a66c96881df557bfba67952
armirqurdina00 <armirqurdina@gmail.com>
2023-04-01 11:12:36 +0200

    implement user authentication

--- main/views.py
+++ main/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return HttpResponse("Hello, world!")
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Welcome!')
+            return redirect('home')
+        else:
+            messages.success(request, 'There was an error logging in, please try again.')
+            return redirect('home')
+    else:    
+        return render(request, "home.html", {})
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
