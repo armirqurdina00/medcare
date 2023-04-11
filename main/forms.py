635dbfd71ae00d363d4db54dd368cd94eab06af0
armirqurdina00 <armirqurdina@gmail.com>
2023-04-11 14:37:12 +0200

    update AddPatientForm

--- main/forms.py
+++ main/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Patient
 
 class AddPatientForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "First Name", "class": "form-control"}), label="")
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Last Name", "class": "form-control"}), label="")
+    phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Phone", "class": "form-control"}), label="")
+    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Email", "class": "form-control"}), label="")
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Address", "class": "form-control"}), label="")
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "City", "class": "form-control"}), label="")
+
     class Meta:
         model = Patient
-        fields = '__all__'
+        exclude = ("user",)
